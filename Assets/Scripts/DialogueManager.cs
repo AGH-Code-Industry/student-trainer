c22d80670101eb96d947354c49b1fa0224fc7e73
qualv13 <71326761+qualv13@users.noreply.github.com>
2023-11-21 00:58:21 +0100

    dialogue system (needs polishing)

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Text nameText;
+    public Text dialogueText;
+
+    public Animator animator;
+    
+    private Queue<string> sentences;
+
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        animator.SetBool("IsOpen", true);
+        
+        Debug.Log("Starting convo with " + dialogue.name);
+        nameText.text = dialogue.name;
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+        //Debug.Log(sentence);
+
+        //dialogueText.text = sentence;
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    void EndDialogue()
+    {
+        animator.SetBool("IsOpen", false);
+        Debug.Log("End of convo");
+    }
+}
