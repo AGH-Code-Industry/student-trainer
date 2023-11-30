8df6bb4bd5631399a3c93f364ef5ab1206756c5b
qualv13 <71326761+qualv13@users.noreply.github.com>
2023-11-30 01:12:39 +0100

    names can queue also
    names can change with dialogue and dialogue starts on trigger with player

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -5,9 +5,8 @@ using UnityEngine;
 [System.Serializable]
 public class Dialogue
 {
-    public string name;
+    public string[] name;
     
     [TextArea(3, 10)]
     public string[] sentences;
-
 }
