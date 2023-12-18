62fc3ea099bd0b5d12f7724bdfb564ad31480d80
Gekon <jakubocytko@student.agh.edu.pl>
2023-12-18 19:51:20 +0100

    add new dialog

---
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Dialogue", menuName = "ScriptableObjects/Dialogue", order = 1)]
+public class Dialogue : ScriptableObject
+{
+	public string characterName;
+
+	[TextArea(3, 10)]
+	public string[] sentences;
+
+}
