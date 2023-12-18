a62f03fbe68193855b5282d227a8838c8d5b002a
Gekon <jakubocytko@student.agh.edu.pl>
2023-12-18 20:20:15 +0100

    fix dialogue system

--- Assets/Scripts/Dialogue/DialogueTrigger.cs
+++ Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -6,6 +6,7 @@ public class DialogueTrigger : MonoBehaviour {
 
 	public Dialogue dialogue;
 
+	[ContextMenu("TriggerDialogue")]
 	public void TriggerDialogue ()
 	{
 		FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
