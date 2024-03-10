22c8736538a6b93729ac9d0c7d611bcab8ddaa42
Kamil Mróz <kamil.2003.kmz@gmail.com>
2024-03-11 00:31:34 +0100

    dodanie ekwipunku
    Stworzenie UI ekwipunku oraz dodanie obsługi wyświetlania go

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,10 +1,10 @@
 using Chapter.Singleton;
 using UnityEngine;
 
-public class InputManager : Singleton<InputManager> 
+public class InputManager : Singleton<InputManager>
 {
     CustomActions input;
-
+    InventoryManager inventoryManager;
     public CustomActions GetInput() => input ??= CreateInput();
 
     private CustomActions CreateInput()
@@ -14,8 +14,20 @@ public class InputManager : Singleton<InputManager>
         return input;
     }
 
-    private void OnDestroy() {
+    private void OnDestroy()
+    {
         input?.Dispose();
     }
-
+    private void Start()
+    {
+        inventoryManager = new InventoryManager();
+        inventoryManager.OnStart();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            inventoryManager.ChangeState();
+        }
+    }
 }
